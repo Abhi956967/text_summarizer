e03ae8149d91a68c018fe20ce948c3ebe0627cac
Abhishek Maurya <abhishek.bcastudent21.25528@cimage.in>
2026-02-12 21:18:51 +0530

    Model Trainer Completed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.text_summarizer.logging import logger
 from src.text_summarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.text_summarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
-# from src.text_summarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.text_summarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 # from src.text_summarizer.pipeline.stage_4_model_evaluation import ModelEvaluationTrainingPipeline
 STAGE_NAME="Data Ingestion stage"
@@ -29,16 +29,17 @@ except Exception as e:
 
 
 
-# STAGE_NAME="Model Trainer stage"
+STAGE_NAME="Model Trainer stage"
 
-# try:
-#     logger.info(f"stage {STAGE_NAME} initiated")
-#     model_trainer_pipeline=ModelTrainerTrainingPipeline()
-#     model_trainer_pipeline.initiate_model_trainer()
-#     logger.info(f"Stage {STAGE_NAME} Completed")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    model_trainer_pipeline=ModelTrainerTrainingPipeline()
+    model_trainer_pipeline.initiate_model_trainer()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
 
 # STAGE_NAME = "Model Evaluation stage"
 # try: 
